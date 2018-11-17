ca9b4cb403b658d2baa1d6380c4a71aeb639a082
StarsNetworkk <44710972+StarsNetworkk@users.noreply.github.com>
2018-11-17 06:38:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -939,7 +939,17 @@ Role : __${ar[message.guild.id].role}__`)
 
 
 
-
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("494920525051592705");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
 
 	
 
